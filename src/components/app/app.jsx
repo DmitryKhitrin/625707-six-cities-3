50189f0f4adedeaacdb59802f87b74f9b80fa95e
Dmitry Khitrin <khitrind@yandex-team.ru>
2020-02-06 23:41:53 +0300

    Added card component and props with props-types.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import {Main} from '../main/main.jsx';
+import {PropTypes} from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
-export const App = ({rentCount}) => {
+
+export const App = ({rentCount, placeCardsList}) => {
   return (
-    <>
-      <Main rentCount={rentCount} />;
-    </>
+    <Main rentCount={rentCount} placeCardsList={placeCardsList}/>
   );
+};
+
+App.propTypes = {
+  placeCardsList: PropTypes.arrayOf(
+      PropTypes.shape({
+        priceValue: PropTypes.number,
+        placeCardImage: PropTypes.string,
+        cardName: PropTypes.string,
+        starsRating: PropTypes.string,
+        roomType: PropTypes.string,
+        isPremium: PropTypes.bool,
+      })
+  ).isRequired,
+  rentCount: PropTypes.number.isRequired
 };
