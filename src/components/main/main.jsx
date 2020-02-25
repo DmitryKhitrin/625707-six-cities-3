70bd9928ac3aba5ebeff657b68fe78b6a18bffaf
Dmitry Khitrin <khitrind@yandex-team.ru>
2020-02-25 14:19:49 +0300

    Added marker highliting.

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,17 +3,34 @@ import {PropTypes} from 'prop-types';
 import {OffersList} from "../offers-list/offers-list.jsx";
 import {Map} from "../map/map.jsx";
 import {CitiesTabsList} from "../cities-tabs-list/cities-tabs-list.jsx";
+import {PlacesSorting} from "../places-sorting/places-sorting.jsx";
 
 class Main extends React.Component {
-
   constructor(props) {
     super(props);
+    this.state = {
+      activePlaceCard: null
+    };
+    this._setActivePlaceCard = this._setActivePlaceCard.bind(this);
+    this._removeActivePlaceCard = this._removeActivePlaceCard.bind(this);
   }
 
   componentDidMount() {
     const {city, getOffers, getLocations} = this.props;
     getOffers(city);
     getLocations();
+  }
+
+  _setActivePlaceCard(id) {
+    this.setState({
+      activePlaceCard: id
+    });
+  }
+
+  _removeActivePlaceCard() {
+    this.setState({
+      activePlaceCard: null
+    });
   }
 
   render() {
@@ -23,7 +40,7 @@ class Main extends React.Component {
       locations,
       setCity,
       getOffers,
-      city,
+      city
     } = this.props;
 
     const place = locations.find((cityInfo) => cityInfo.cityName === city);
@@ -46,53 +63,15 @@ class Main extends React.Component {
                 Places
               </h2>
               <b className="places__found">{`${placeCardsList.length} places to stay in ${city}`}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex="0"
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex="0">
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex="0">
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex="0">
-                    Top rated first
-                  </li>
-                </ul>
-
-                <select className="places__sorting-type" id="places-sorting">
-                  <option className="places__option" value="popular">
-                    Popular
-                  </option>
-                  <option className="places__option" value="to-high">
-                    Price: low to high
-                  </option>
-                  <option className="places__option" value="to-low">
-                    Price: high to low
-                  </option>
-                  <option className="places__option" value="top-rated">
-                    Top rated first
-                  </option>
-                </select>
-              </form>
+              <PlacesSorting />
               {placeCardsList.length === 0 ? (
                 `No places to stay available`
               ) : (
                 <OffersList
                   placeCardsList={placeCardsList}
                   onHeaderClick={onHeaderClick}
+                  onMouseEnter={this._setActivePlaceCard}
+                  onMouseLeave={this._removeActivePlaceCard}
                 />
               )}
             </section>
@@ -103,6 +82,7 @@ class Main extends React.Component {
                     city={location}
                     placeCardsList={placeCardsList}
                     height={1000}
+                    activeCard={this.state.activePlaceCard}
                   />
                 ) : null}
               </section>
