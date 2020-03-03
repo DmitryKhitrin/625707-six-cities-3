bef6af5c95f4b38edabdb28fa557b36c95f4314a
Dmitry Khitrin <khitrind@yandex-team.ru>
2020-03-03 17:28:57 +0300

    Removed mocks.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
-import {offers} from "./mocks/offers.js";
 import {offerPropperties} from "./mocks/offer-propperties.js";
 import {App} from './components/app/app.jsx';
 import {store} from "./redux/index.js";
@@ -11,7 +10,6 @@ const onHeaderClick = () => {};
 ReactDOM.render(
     <Provider store={store}>
       <App
-        placeCardsList={offers}
         onHeaderClick={onHeaderClick}
         offerPropperties={offerPropperties}
       />,
