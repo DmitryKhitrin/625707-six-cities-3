04c8dd0ec1aa7e868423e6bab534627314b703e3
Dmitry Khitrin <khitrind@yandex-team.ru>
2020-03-08 15:16:33 +0300

    Updated reducer test.

--- src/redux/offers/offer-reducer.test.js
+++ src/redux/offers/offer-reducer.test.js
@@ -1,9 +1,49 @@
 import {offersReducer} from "./offer-reducer.js";
-import {SET_CITY} from "./types.js";
+import {SET_CITY, UPDATE_OFFERS_INFO, SET_FAVORITE, SET_OFFERS} from "./types.js";
 
 describe(`offerReducer`, () => {
-  it(`should set city.`, () => {
+  it(`should SET_CITY.`, () => {
     const state = offersReducer({}, {type: SET_CITY, payload: `Moscow`});
     expect(state).toEqual({city: `Moscow`});
+  });
+
+  it(`should UPDATE_OFFERS_INFO.`, () => {
+    const state = offersReducer(
+        {},
+        {type: UPDATE_OFFERS_INFO, payload: {
+          offers: [1, 2, 3],
+        }}
+    );
+    expect(state).toEqual({offers: [1, 2, 3]});
+  });
+
+  it(`should SET_FAVORITE.`, () => {
+    const state = offersReducer(
+        {},
+        {
+          type: SET_FAVORITE,
+          payload: {
+            favorites: [1, 2, 3]
+          }
+        }
+    );
+    expect(state).toEqual({favorites: [1, 2, 3]});
+  });
+
+  it(`should SET_OFFERS.`, () => {
+    const state = offersReducer(
+        {},
+        {
+          type: SET_OFFERS,
+          payload: {
+            locations: [`Moscow`, `Paris`],
+            offers: [1, 2, 3]
+          }
+        }
+    );
+    expect(state).toEqual({
+      locations: [`Moscow`, `Paris`],
+      offers: [1, 2, 3]
+    });
   });
 });
